7172023de15e132b0afa5abd0cd7c9aa9c7ee6eb
hieu vuong <vuonghieu283@gmail.com>
2022-05-31 00:46:22 +0700

    first commit

---
+++ src/main/java/com/example/Register/t2009m1helloworld/controller/WebController.java
@@ -0,0 +1,20 @@
+package com.example.Register.t2009m1helloworld.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class WebController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
